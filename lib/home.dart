2d9c5ff0a0fb6eb83ea84483ae68d4f4baeb7d99
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2026-02-24 12:07:35 +0700

    feat : home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:ai_chat_bot/login.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,6 +11,42 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("HOME")));
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(top: 16.0), // ขยับลง 16 px
+          child: Image.asset("assets/images/logo.png", width: 100, height: 100),
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              constraints: const BoxConstraints(), // ลบขนาดจำกัด
+              icon: Image.asset(
+                "assets/images/profile.png",
+                width: 50,
+                height: 50,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10), // ระยะซ้ายสุด
+          child: IconButton(
+            onPressed: () {},
+            padding: EdgeInsets.zero, // ลบ padding ภายใน
+            constraints: const BoxConstraints(), // ลบข้อจำกัด
+            icon: Image.asset(
+              "assets/images/bank.png",
+              width: 50, // ขนาดเต็มตามต้องการ
+              height: 50,
+            ),
+          ),
+        ),
+      ),
+      body: Center(child: Text("HOME")),
+    );
   }
 }
